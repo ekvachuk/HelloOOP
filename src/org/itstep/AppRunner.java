3f506e9bd38188c480641050b33c713ab4d2936b
ekvachuk <jekvach@ukr.net>
2019-05-23 21:10:51 +0300

    Add new classes

---
+++ src/org/itstep/AppRunner.java
@@ -0,0 +1,14 @@
+package org.itstep;
+
+public class AppRunner {
+    public static void main(String[] args) {
+    User me = new User();
+
+    me.name = "Alex";
+    me.pasword = "12345";
+
+    User you = new User();
+    you.name = "Your name";
+
+    }
+}
